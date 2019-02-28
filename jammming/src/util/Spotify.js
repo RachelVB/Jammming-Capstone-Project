0b41ec492af83e8b4517a40368de9e0835a6b4fa
RachelVB <vanbuskirk2014@hotmail.com>
2019-02-28 09:13:39 -0800

    Promise bug fix

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -50,23 +50,34 @@ const Spotify = {
   The logging (catching errors) is stripped away for clarity but you get the basic idea. 
   Get it to work with promises and then go back and refactor with async-await. */
   savePlaylist(name, trackUris) {
+
     if (!name || !trackUris.length) {
       return;
     }
-
     const accessToken = Spotify.getAccessToken();
     const headers = { Authorization: `Bearer ${accessToken}` };
     let userId;
 
-    return fetch('https://api.spotify.com/v1/me', {headers: headers}
-    ).then(response => response.json()
+    return fetch('https://api.spotify.com/v1/me', {
+      headers: headers}
+    ).then(response => {
+      if (response.ok) {
+        response.json()
+      }
+      throw new Error('Request Failed!');
+    }, networkError => console.log(networkError.message)
     ).then(jsonResponse => {
       userId = jsonResponse.id;
       return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
         headers: headers,
         method: 'POST',
         body: JSON.stringify({name: name})
-      }).then(response => response.json()
+      }).then(response => {
+        if (response.ok) {
+          response.json()
+        }
+        throw new Error('Request Failed!');
+      }, networkError => console.log(networkError.message)
       ).then(jsonResponse => {
         const playlistId = jsonResponse.id;
         return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
